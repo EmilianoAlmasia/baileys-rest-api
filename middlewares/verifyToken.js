a82ee81261dafb891edec2b2abc7c7634ecc5850
EmilianoAlmasia <ealmasia@hwapplications.com>
2025-06-13 17:15:09 -0300

    Fix: JWT middleware now uses Authorization header

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,19 +1,24 @@
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 const { ACCESS_TOKEN_SECRET } = process.env;
 
-const verifyToken = async (req, res, next) => {
-  if (!req.headers['x-access-token']) {
-    res.sendError(401, 'Unauthorized access: No token provided');
-    return;
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; // Separa "Bearer <token>"
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized access: No token provided' });
   }
 
-  if (req.headers['x-access-token'] !== ACCESS_TOKEN_SECRET) {
-    res.sendError(401, 'Unauthorized access: Invalid token');
-    return;
+  try {
+    const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET);
+    req.user = decoded; // opcional: guardar info del token
+    next();
+  } catch (err) {
+    return res.status(403).json({ message: 'Invalid token' });
   }
-
-  next();
 };
 
 module.exports = verifyToken;
+
